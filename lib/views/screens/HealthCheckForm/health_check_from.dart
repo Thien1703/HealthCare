2adfa9435983e0f83bd8c506de6e94b00d2245c8
huynhminhkhai <150777715+huynhminhkhai@users.noreply.github.com>
2025-01-12 13:12:02 +0700

    add/HealthCheckFrom

---
+++ lib/views/screens/HealthCheckForm/health_check_from.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class HealthCheckFrom extends StatelessWidget {
+  const HealthCheckFrom({super.key});
+
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
