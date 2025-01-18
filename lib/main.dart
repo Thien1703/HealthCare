42d5b4daa27a1698660d37effd5927c8caede714
dnkkhan18 <dngkhanh3210@gmail.com>
2025-01-18 12:21:50 +0700

    dialog_date...

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 // ignore: depend_on_referenced_packages
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:health_care/views/screens/welcome/splash_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: "AIzaSyCqzgAi64GAYpZZ_pkEwriIV4-RFuYEgWM",
+      appId: "1:357055052387:android:3ca405edbbe269ed929656",
+      messagingSenderId: "357055052387",
+      projectId: "health-care-a85f5",
+      storageBucket: "health-care-a85f5.firebasestorage.app",
+    ),
+  );
   runApp(const MyApp());
 }
 
@@ -14,7 +25,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const SplashScreen(),
+      home: SplashScreen(),
       localizationsDelegates: [
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
--- lib/views/screens/welcome/splash_screen.dart
+++ lib/views/screens/welcome/splash_screen.dart
@@ -1,72 +1,3 @@
-/*import 'package:flutter/material.dart';
-import 'package:health_care/views/screens/home/home_screens.dart';
-import 'package:health_care/views/screens/welcome/welcome_screen.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-
-class SplashScreen extends StatefulWidget {
-  const SplashScreen({super.key});
-
-  @override
-  State<SplashScreen> createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen> {
-  @override
-  void initState() {
-    super.initState();
-    _checkFirstTime();
-  }
-
-  Future<void> _checkFirstTime() async {
-    final prefs = await SharedPreferences.getInstance();
-    final isFirstTime = prefs.getBool('isFirstTime') ?? true;
-    Future.delayed(const Duration(seconds: 3), () {
-      
-      if (isFirstTime) {
-        prefs.setBool('isFirstTime', false);
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(builder: (context) => WelcomeScreen()),
-        );
-      } else {
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(builder: (context) => HomeScreens()),
-        );
-      }
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.green,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image.asset(
-              'assets/images/Frame 6.png',
-              width: 128,
-              height: 128,
-            ),
-            const SizedBox(height: 20.0),
-            const Text(
-              'Your Health Is Our Priority',
-              style: TextStyle(
-                fontSize: 24.0,
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-*/
-
 //Test
 import 'package:flutter/material.dart';
 import 'package:health_care/views/screens/welcome/welcome_screen.dart';
@@ -86,7 +17,6 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 
   Future<void> _checkFirstTime() async {
-    // Bỏ kiểm tra trạng thái và chuyển trực tiếp tới WelcomeScreen
     Future.delayed(const Duration(seconds: 3), () {
       Navigator.pushReplacement(
         context,
@@ -104,7 +34,7 @@ class _SplashScreenState extends State<SplashScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Image.asset(
-              'assets/images/Frame 6.png',
+              'assets/images/Frame.png',
               width: 128,
               height: 128,
             ),
