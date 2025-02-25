ec26e0f2a30e7829c6051995b230205086170de0
dnkkhan18 <dngkhanh3210@gmail.com>
2025-02-25 20:32:25 +0700

    luu chuyen nhanh

--- lib/viewmodels/api_service.dart
+++ lib/viewmodels/api_service.dart
@@ -3,7 +3,7 @@ import 'package:health_care/services/local_storage_service.dart';
 import 'package:http/http.dart' as http;
 
 class ApiService {
-  static const String baseUrl = 'http://192.168.3.100:8080/api/v1/auth';
+  static const String baseUrl = 'http://192.168.1.5:8080/api/v1/auth';
 
   // Đăng nhập
   static Future<String?> login(String phoneNumber, String password) async {
